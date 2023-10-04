3be812b5f815628e35649eb63bd003e726fd40ad
SazifRahman <sazifrahman3@gmail.com>
2023-10-04 12:51:57 +0600

    Admin Panel

---
+++ functions/function.php
@@ -0,0 +1,17 @@
+<?php
+
+        include_once('config.php');
+
+    function get_header(){
+        include_once('includes/header.php');
+    }
+
+    function get_sidebar(){
+        include_once('includes/sidebar.php'); 
+    }
+
+    function get_footer(){
+        include_once('includes/footer.php');
+    }
+
+?>
