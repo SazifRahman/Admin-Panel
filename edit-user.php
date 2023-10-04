ebeefd23bae84868c2989616a7c4e51d781ebb81
SazifRahman <sazifrahman3@gmail.com>
2023-10-04 16:55:19 +0600

    Admin Panel

--- edit-user.php
+++ edit-user.php
@@ -4,69 +4,73 @@ include_once('functions/function.php');
 get_header();
 get_sidebar();
 
-// if (!empty($_POST)) {
+$id=$_GET['e'];
+$sel="SELECT * FROM users WHERE user_id='$id'";
+$Q=mysqli_query($con,$sel);
+$data=mysqli_fetch_assoc($Q);
 
-//   $name = $_POST['name'];
-//   $phone = $_POST['phone'];
-//   $email = $_POST['email'];
-//   $username = $_POST['username'];
-//   $password = md5(($_POST['password']));
-//   $repassword = md5(($_POST['repassword']));
-//   $role = $_POST['role'];
+if (!empty($_POST)) {
+
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+  // $username = $_POST['username'];
+  // $password = md5(($_POST['password']));
+  // $repassword = md5(($_POST['repassword']));
+  $role = $_POST['role'];
+  $image = $_FILES['image'];
+  // $imageName = '';
+  if ($image['name'] != '') {
+
+    // $imageName = 'user_' . time() . '_' . rand(100000, 10000000) . '.' . pathinfo($image['name'], PATHINFO_EXTENSION);
+  }
+    // $insert_query = "INSERT INTO `users`( `user_name`, `user_phone`, `user_email`, `user_username`, `user_password`, `role_id`, `user_photo`) 
+    // VALUES ('$name','$phone','$email','$username','$password','$role','$imageName')";
+
+    $update="UPDATE users SET user_name='$name', user_phone='$phone', user_email='$email', role_id='$role', WHERE user_id='$id'";
+
+  if (!empty($name)) {
+    if (!empty($phone)) {
+      if (!empty($email)) {
+        // if (!empty($username)) {
+        //   if (!empty($password)) {
+        //     if (!empty($repassword)) {
+        //       if ($password === $repassword) {
+                if (!empty($role)) {
 
 
+                  if (mysqli_query($con, $insert_update)) {
+                    move_uploaded_file($image['tmp_name'], 'uploads/' . $imageName);
 
-//   $image = $_FILES['image'];
-//   $imageName = '';
-//   if ($image['name'] != '') {
-
-//     $imageName = 'user_' . time() . '_' . rand(100000, 10000000) . '.' . pathinfo($image['name'], PATHINFO_EXTENSION);
-//   }
-//     $insert_query = "INSERT INTO `users`( `user_name`, `user_phone`, `user_email`, `user_username`, `user_password`, `role_id`, `user_photo`) 
-//     VALUES ('$name','$phone','$email','$username','$password','$role','$imageName')";
-
-//   if (!empty($name)) {
-//     if (!empty($phone)) {
-//       if (!empty($email)) {
-//         if (!empty($username)) {
-//           if (!empty($password)) {
-//             if (!empty($repassword)) {
-//               if ($password === $repassword) {
-//                 if (!empty($role)) {
-
-
-//                   if (mysqli_query($con, $insert_query)) {
-//                     move_uploaded_file($image['tmp_name'], 'uploads/' . $imageName);
-
-//                     echo "User insert successfully";
-//                   } else {
-//                     echo "Something is worng";
-//                   }
-//                 } else {
-//                   echo "OPPS! Please Select Your Role.";
-//                 }
-//               } else {
-//                 echo "Password & Repassword Didn't Match.";
-//               }
-//             } else {
-//               echo "OPPS! Enter Your Password.";
-//             }
-//           } else {
-//             echo "OPPS! Enter Your Password.";
-//           }
-//         } else {
-//           echo "OPPS! Enter Your Username.";
-//         }
-//       } else {
-//         echo "OPPS! Enter Your Email.";
-//       }
-//     } else {
-//       echo "OPPS! Enter Your Phone.";
-//     }
-//   } else {
-//     echo "OPPS! Enter Your Name.";
-//   }
-// }
+                    echo "User insert successfully";
+                  } else {
+                    echo "Something is worng";
+                  }
+                } else {
+                  echo "OPPS! Please Select Your Role.";
+                }
+      //         } else {
+      //           echo "Password & Repassword Didn't Match.";
+      //         }
+      //       } else {
+      //         echo "OPPS! Enter Your Password.";
+      //       }
+      //     } else {
+      //       echo "OPPS! Enter Your Password.";
+      //     }
+      //   } else {
+      //     echo "OPPS! Enter Your Username.";
+      //   }
+      // } else {
+        echo "OPPS! Enter Your Email.";
+      }
+    } else {
+      echo "OPPS! Enter Your Phone.";
+    }
+  } else {
+    echo "OPPS! Enter Your Name.";
+  }
+}
 
 ?>
 
@@ -91,25 +95,25 @@ get_sidebar();
           <div class="row mb-3">
             <label class="col-sm-3 col-form-label col_form_label">Name<span class="req_star">*</span>:</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control form_control" id="" name="name">
+              <input type="text" class="form-control form_control" id="" name="name" value="<?=$data['user_name'];?>">
             </div>
           </div>
           <div class="row mb-3">
             <label class="col-sm-3 col-form-label col_form_label">Phone<span class="req_star">*</span>:</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control form_control" id="" name="phone">
+              <input type="text" class="form-control form_control" id="" name="phone" value="<?=$data['user_phone'];?>">
             </div>
           </div>
           <div class="row mb-3">
             <label class="col-sm-3 col-form-label col_form_label">Email<span class="req_star">*</span>:</label>
             <div class="col-sm-7">
-              <input type="email" class="form-control form_control" id="" name="email">
+              <input type="email" class="form-control form_control" id="" name="email" value="<?=$data['user_email'];?>">
             </div>
           </div>
           <div class="row mb-3">
             <label class="col-sm-3 col-form-label col_form_label">Username<span class="req_star">*</span>:</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control form_control" id="" name="username">
+              <input type="text" class="form-control form_control" id="" name="username" value="<?=$data['user_username'];?>" disabled>
             </div>
           </div>
           <!-- <div class="row mb-3">
@@ -135,7 +139,7 @@ get_sidebar();
                 $Qr = mysqli_query($con, $selr);
                 while ($urole = mysqli_fetch_assoc($Qr)) {
                 ?>
-                  <option value="<?= $urole['role_id']; ?>"><?= $urole['role_name']; ?></option>
+                  <option value="<?= $urole['role_id'];?>"> <?php if($urole['role_id']==$data['role_id']){ echo '(selected)';} ?> <?= $urole['role_name']; ?></option>
                 <?php } ?>
 
               </select>
@@ -145,6 +149,15 @@ get_sidebar();
             <label class="col-sm-3 col-form-label col_form_label">Photo:</label>
             <div class="col-sm-4">
               <input type="file" class="form-control form_control" id="" name="image">
+            </div>
+            <div class="col-md-2">
+            
+            <?php if($data['user_photo']!=''){ ?>
+                <img height="100" src="uploads/<?= $data['user_photo']; ?>" alt="User"/> 
+                <?php }else{?>
+                <img height="100" class="img200" src="images/avatar.jpg" alt="User"/>
+                <?php }?>
+
             </div>
           </div>
         </div>
